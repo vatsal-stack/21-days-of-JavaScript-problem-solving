eb9295255b5df7dcf49010739b8920703072666f
vatsal-stack <iamvatsalpanchal@gmail.com>
2026-07-15 13:25:56 +0530

    done

--- day-1/p1.js
+++
@@ -1,9 +0,0 @@
-// Print Numbers from 1 to N
-
-function solve(n) {
-  for (let i = 1; i <= n; i++) {
-    console.log(i);
-  }
-}
-
-solve(12);
---
+++ p1.js
@@ -0,0 +1,7 @@
+// sum of two integer
+
+let a = 10;
+let b = 20;
+
+let c = a + b;
+console.log(c);
